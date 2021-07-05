17c0d3f5daf1a7e8ef3ec46fda2fd4af3671a43d
zachkirkrey <zachkirkrey@gmail.com>
2021-07-05 20:51:48 +0530

    websocket server

---
+++ server.js
@@ -0,0 +1,43 @@
+const WebSocket = require('ws');
+const redis = require('redis');
+
+
+// Configuration: adapt to your environment
+const REDIS_SERVER = "redis://localhost:6379";
+const WEB_SOCKET_PORT = 8005;
+
+// Connect to Redis and subscribe to "app:notifications" channel
+var redisClient = redis.createClient(REDIS_SERVER);
+redisClient.subscribe('BO-DATA');
+
+var client = redis.createClient(REDIS_SERVER);
+
+// Create & Start the WebSocket server
+const server = new WebSocket.Server({ port: WEB_SOCKET_PORT });
+
+
+
+// Register event for client connection
+server.on('connection', function connection(ws) {
+
+    client.get("BO-DATA", (err, data) => {
+        if (err) {
+            console.error(err);
+            // throw err;
+        }
+        else {
+            console.log("1. Get key from redis - ", data);
+            ws.send(data);
+        }
+
+    });
+
+    // broadcast on web socket when receving a Redis PUB/SUB Event
+    redisClient.on('message', function (channel, message) {
+        console.log(message);
+        ws.send(message);
+    })
+
+});
+
+console.log("WebSocket server started at ws://locahost:" + WEB_SOCKET_PORT);
