5deaacc4d03401652fd95484951451c400406006
sbryson630 <sbryson630@gmail.com>
2021-07-12 22:49:46 +0530

    code updates

--- cron.py
+++ cron.py
@@ -16,8 +16,12 @@ rclient = redis.StrictRedis(connection_pool=USERPOOL, decode_responses=True)
 
 CURR_TIME = datetime.now()
 
-FIXTURE_TIME = CURR_TIME + timedelta(seconds=18)
+FIXTURE_TIME = CURR_TIME + timedelta(seconds=18000)
 print(FIXTURE_TIME)
+
+fixtures = rclient.get('fixtureCreated')
+print(fixtures)
+
 
 if rclient.get("fixtureCreated") == None:
     
@@ -46,37 +50,71 @@ if rclient.get("fixtureCreated") == None:
 
     print(res)
 
-
-    SECRET_KEY = '8sxy54vjd3ks5cge'
-
-    access_key = hashlib.md5((SECRET_KEY+str(res)).encode('utf-8')).hexdigest()
-
+    SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
+    access_key = hashlib.md5((SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
     print('ACCESS KEY: ', access_key)
-
-    headers = {"content-type": "application/json",
-               "oneworks-access-key": access_key}
-
+    headers = {"content-type": "application/json","oneworks-access-key": access_key}
     print(headers)
-
     response = requests.post("http://owapi1.playthefun.com:9130/api/CryptoCurrency/CreateFixture", json=res, headers=headers)
     print(response)
-    print(response.json())
-    print(json.dumps(response.json()))
-    print('\n\n')
- 
-    if(CURR_TIME.timestamp() < fixtures[0]['startTime']):
+    if(response.status_code == 200):
+        rclient.set("fixtureCreated", str(fixtures))
+
+
+else:
+    fixtures = rclient.get('fixtureCreated')
+    if (fixtures):
+        fixtures = ast.literal_eval(fixtures)
+    
+    print(fixtures)
+    print(CURR_TIME.timestamp())
+
+    curr_time = int(CURR_TIME.timestamp() * 1000)
+    print(curr_time)
+
+    if(curr_time < fixtures[0]['startTime']):
         print('WAIT 1')
         print(fixtures[0]['id'])
-    if(CURR_TIME.timestamp() > fixtures[0]['startTime'] and CURR_TIME.timestamp() < fixtures[0]['marketEndTime']):
+    if(curr_time > fixtures[0]['startTime'] and curr_time < fixtures[0]['marketEndTime']):
         print('WAIT 2')
         print(fixtures[0]['id'])
-    if(CURR_TIME.timestamp() > fixtures[0]['marketEndTime'] and CURR_TIME.timestamp() < fixtures[0]['endTime']):
+    if(curr_time > fixtures[0]['marketEndTime'] and curr_time < fixtures[0]['endTime']):
         print('PUSH FIXTURE ID TO JSON')
         print(fixtures[0]['id'])
-    if(CURR_TIME.timestamp() > fixtures[0]['endTime']):
+        rclient.set('fixtureId', str(fixtures[0]['id']))
+
+    if(curr_time > fixtures[0]['endTime']):
         print('CALL END FIXTURE API AND COMMUNICATE THE RESULT')
         print(fixtures[0]['id'])
-    # rclient.set('last_sent_price', str(mark_price))
-    # rclient.setex("fixtureCreated", 60, 1)
 
+        res = {}
 
+        seq = str(uuid.uuid4())
+        print(seq)
+
+        resdisdata = rclient.get('last_sent_price')
+        if (resdisdata):
+            last_sent_price = ast.literal_eval(resdisdata)
+
+        if(last_sent_price > 0):
+            res['Timestamp'] = datetime.now().strftime('%Y/%m/%d %I:%M:%S')
+            res['Seq'] = seq
+        
+            res['Fixture'] = {
+                "Id": fixtures[0]['id'],
+                "Price": last_sent_price,
+                "Status": 0
+            }
+
+            print(res)
+
+            SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
+            access_key = hashlib.md5((SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
+            print('ACCESS KEY: ', access_key)
+            headers = {"content-type": "application/json","oneworks-access-key": access_key}
+            print(headers)
+            response = requests.post("http://owapi1.playthefun.com:9130/api/CryptoCurrency/EndFixture", json=res, headers=headers)
+            print(response)
+            if(response.status_code == 200):
+                rclient.delete("fixtureCreated")
+                rclient.delete("fixtureId")
