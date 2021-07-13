e19864dc2dd5fd03f4bfb33dfce2700666df16b2
zachkirkrey <zachkirkrey@gmail.com>
2021-07-13 18:50:57 +0530

    cron code with fixture json

--- createJsonBackup.py
+++ createJsonBackup.py
@@ -13,7 +13,6 @@ USERPOOL = redis.ConnectionPool(
     host='localhost', port=6379, db=0, decode_responses=True)
 rclient = redis.StrictRedis(connection_pool=USERPOOL, decode_responses=True)
 
-
 def nearest(list, timestamp):
     return min([t for t in list if t > timestamp])
 
@@ -22,17 +21,15 @@ def findNearest(min_time_exp, time_roundings, curr_datetime):
     min_time_rounding = min(time_roundings)
 
     print("TIME ROUNDING: ", min_time_rounding)
-
-    min_minutes_diff = (min_time_rounding -
-                        curr_datetime).total_seconds() / 60.0
+    
+    min_minutes_diff = (min_time_rounding - curr_datetime).total_seconds() / 60.0
     print("TIME DIFF: ", min_minutes_diff)
 
     if(len(time_roundings) > 1):
         while(min_time_exp > min_minutes_diff):
             min_time_rounding = nearest(time_roundings, min_time_rounding)
             print('NEXT TIME ROUNDING: ', min_time_rounding)
-            min_minutes_diff = (min_time_rounding -
-                                curr_datetime).total_seconds() / 60.0
+            min_minutes_diff = (min_time_rounding - curr_datetime).total_seconds() / 60.0
             print("TIME DIFF: ", min_minutes_diff)
             if(min_time_exp > min_minutes_diff):
                 break
@@ -48,25 +45,24 @@ def get_config_data():
     data = json.load(f)
     return data
 
-
-async def calculate(data, PRICE, fixtureId):
+async def calculate(data,PRICE):
     SLOTS = 2
     EXPIRIES = []
     last_timeline = None
 
-    print('INPUT DATA: ', data)
+    
+    print('INPUT DATA: ',data)
     print('\n\n')
 
-    print('PRICE: ', PRICE)
+    print('PRICE: ',PRICE )
     print('\n')
-
+   
     try:
-        for i in range(1, SLOTS+1):
+        for i in range(1,SLOTS+1):
             try:
                 exp = {}
 
-                print('-------------------SLOT-' +
-                      str(i)+'-------------------------')
+                print('-------------------SLOT-'+str(i)+'-------------------------')
                 slot_left = data['Slots_left_'+str(i)]
                 slot_right = data['Slots_right_'+str(i)]
                 slot_rounding = data['Slot_Rounding_'+str(i)]
@@ -89,7 +85,7 @@ async def calculate(data, PRICE, fixtureId):
                 # print(nearest_over)
                 rounding_left.append(nearest_under)
                 rounding_right.append(nearest_over)
-
+                
                 for j in range(0, slot_left-1):
                     last_under = rounding_left[-1]
                     next_under = last_under - rounding_min
@@ -97,12 +93,13 @@ async def calculate(data, PRICE, fixtureId):
 
                 rounding_left = sorted(rounding_left, key=int)
                 print("ROUNDING LEFT: ", rounding_left)
-
+                
+                
                 for j in range(0, slot_right-1):
                     last_over = rounding_right[-1]
                     next_over = last_over + rounding_min
                     rounding_right.append(next_over)
-
+                
                 print("ROUNDING RIGHT: ", rounding_right)
 
                 strike_prices = rounding_left + rounding_right
@@ -120,6 +117,7 @@ async def calculate(data, PRICE, fixtureId):
                     # print(ct)
                     # print(type(ct))
 
+
                     timezone_rounding = data['Timezone_Rounding_'+str(i)]
 
                     timestamps = []
@@ -131,19 +129,18 @@ async def calculate(data, PRICE, fixtureId):
                             hour = curr_hour
                             minute = res
                             min_diff = res - curr_minute
-                            print("MIN DIFF: ", min_diff)
-                            next_datetime = curr_datetime + \
-                                timedelta(minutes=min_diff)
+                            print("MIN DIFF: ",min_diff)
+                            next_datetime = curr_datetime + timedelta(minutes=min_diff)
                         elif(curr_minute > res):
                             hour = curr_hour + 1
                             minute = res
-
+                            
                             min_diff = res - curr_minute
                             print("MIN DIFF: ", min_diff)
                             total_diff = 60 + min_diff
                             print("MIN DIFF: ", total_diff)
-                            next_datetime = curr_datetime + \
-                                timedelta(minutes=total_diff)
+                            next_datetime = curr_datetime + timedelta(minutes=total_diff)
+
 
                         # print(hour, minute)
                         next_time = str(hour)+":"+str(minute)
@@ -156,8 +153,7 @@ async def calculate(data, PRICE, fixtureId):
                     min_time_exp = data['Minimum_time_expiration_'+str(i)]
                     print("MIN TIME EXPIRY: ", min_time_exp)
 
-                    min_time_rounding = findNearest(
-                        min_time_exp, time_roundings, curr_datetime)
+                    min_time_rounding = findNearest(min_time_exp, time_roundings, curr_datetime)
                     print("MIN TIMESTAMP: ", min_time_rounding)
                     # min_hour = min_time_rounding.hour
                     # min_minute = min_time_rounding.minute
@@ -178,12 +174,11 @@ async def calculate(data, PRICE, fixtureId):
 
                         last_timeline = min_time_rounding
 
+                        
                         for k in range(0, total_timelines-1):
                             exp = {}
-                            next_min_time_rounding = min_time_rounding + \
-                                timedelta(minutes=timelines_gap)
-                            print("NEXT MIN TIMESTAMP: ",
-                                  next_min_time_rounding)
+                            next_min_time_rounding = min_time_rounding + timedelta(minutes=timelines_gap)
+                            print("NEXT MIN TIMESTAMP: ", next_min_time_rounding)
                             # next_min_hour = next_min_time_rounding.hour
                             # next_min_minute = next_min_time_rounding.minute
                             # next_min_hour_minute = str(next_min_hour)+":"+str(next_min_minute)
@@ -191,8 +186,7 @@ async def calculate(data, PRICE, fixtureId):
                             min_time_rounding = next_min_time_rounding
                             print('\n\n')
 
-                            exp['expiry'] = int(
-                                next_min_time_rounding.timestamp())
+                            exp['expiry'] = int(next_min_time_rounding.timestamp())
                             exp['strikes'] = strike_prices
 
                             EXPIRIES.append(exp)
@@ -200,28 +194,23 @@ async def calculate(data, PRICE, fixtureId):
                             last_timeline = next_min_time_rounding
                 else:
                     if(last_timeline != None):
-                        print("LAST TIMELINE OF SLOT " +
-                              str(i-1)+": ", last_timeline)
+                        print("LAST TIMELINE OF SLOT "+str(i-1)+": " , last_timeline)
 
                         total_timelines = data['Timelines_'+str(i)]
                         timelines_gap = data['Timezone_Gap_'+str(i)]
                         print(timelines_gap)
 
                         if(total_timelines > 0):
-                            next_min_time_rounding = last_timeline + \
-                                timedelta(minutes=timelines_gap)
-                            exp['expiry'] = int(
-                                next_min_time_rounding.timestamp())
+                            next_min_time_rounding = last_timeline + timedelta(minutes=timelines_gap)
+                            exp['expiry'] = int(next_min_time_rounding.timestamp())
                             exp['strikes'] = strike_prices
 
                             EXPIRIES.append(exp)
-
+                        
                             for k in range(0, total_timelines-1):
                                 exp = {}
-                                next_min_time_rounding = next_min_time_rounding + \
-                                    timedelta(minutes=timelines_gap)
-                                print("NEXT MIN TIMESTAMP: ",
-                                      next_min_time_rounding)
+                                next_min_time_rounding = next_min_time_rounding + timedelta(minutes=timelines_gap)
+                                print("NEXT MIN TIMESTAMP: ", next_min_time_rounding)
                                 # next_min_hour = next_min_time_rounding.hour
                                 # next_min_minute = next_min_time_rounding.minute
                                 # next_min_hour_minute = str(next_min_hour)+":"+str(next_min_minute)
@@ -229,14 +218,16 @@ async def calculate(data, PRICE, fixtureId):
                                 # last_timeline = next_min_time_rounding
                                 print('\n\n')
 
-                                exp['expiry'] = int(
-                                    next_min_time_rounding.timestamp())
+                                exp['expiry'] = int(next_min_time_rounding.timestamp())
                                 exp['strikes'] = strike_prices
 
                                 EXPIRIES.append(exp)
 
+
+
             except Exception as e:
                 print(e)
+       
 
         result = {}
 
@@ -250,20 +241,20 @@ async def calculate(data, PRICE, fixtureId):
         # with open('output.json', "w+") as f:
         #     f.write(finalJson)
 
-        response = requests.post(
-            "http://18.183.28.37:3000/odds", data=finalJson)
+        response = requests.post("http://18.183.28.37:3000/odds", data=finalJson)
         # print(response)
         # print(response.json())
         print(json.dumps(response.json()))
         print('\n\n')
+        
 
         finalOutput = {}
         expriries = []
-
+        
         for j in response.json()['expiries']:
             odds_id = 1
             expiry = {}
-            expiry['fixtureId'] = fixtureId
+            expiry['expiry'] = j['expiry']
             probabilities = []
             # idexpiries = db_add_expiries(j['expiry'], PRICE, data['Rake_over'], data['Rake_under'])
             # print(idexpiries)
@@ -271,7 +262,7 @@ async def calculate(data, PRICE, fixtureId):
             for prob in j['probabilities']:
                 probability = {}
                 # print(prob)
-                over_prob = prob['probability']
+                over_prob = prob['probability'] 
                 under_prob = (1 - over_prob)
                 rake_over = (1 - data['Rake_over']) * (1 / over_prob)
                 rake_under = (1 - data['Rake_under']) * (1 / under_prob)
@@ -282,26 +273,24 @@ async def calculate(data, PRICE, fixtureId):
                 probability['under'] = float('{:.3g}'.format(rake_under))
 
                 # db_add_probabilities(idexpiries, odds_id, prob['strike'], float('{:.3g}'.format(over_prob)), float('{:.3g}'.format(under_prob)))
-
+                
                 probabilities.append(probability)
                 odds_id = odds_id + 1
-
+            
             expiry['probabilities'] = probabilities
 
             expriries.append(expiry)
-
+                
         finalOutput['price'] = PRICE
-        finalOutput['timestamp'] = datetime.now().strftime('%Y/%m/%d %I:%M:%S')
-        finalOutput['type'] = 1
         # finalOutput['rake_over'] = data['Rake_over']
         # finalOutput['rake_under'] = data['Rake_under']
-        finalOutput['fixtures'] = expriries
-        
+        finalOutput['expiries'] = expriries
         print(json.dumps(finalOutput))
         print('\n\n')
 
         rclient.set('BO-DATA', json.dumps(finalOutput))
         rclient.publish('BO-DATA', json.dumps(finalOutput))
+
 
         refresh_rate = data['Refresh_rate']
         await asyncio.sleep(refresh_rate)
@@ -311,36 +300,35 @@ async def calculate(data, PRICE, fixtureId):
         return {}
 
 
+
 async def main():
     error = 0
     input_data = get_config_data()
     for k, v in input_data.items():
-        if(type(v) == list):
+        if(type(v)==list):
             flag = 0
             for d in v:
                 if(type(d) == int and d < 0):
                     flag = 1
-
+            
             if(flag == 1):
                 print('Invaid '+k)
                 error = 1
-
+                    
         if(type(v) == int and v < 0):
             print('Invaid '+k)
             error = 1
-
-    if(error == 0):
-        binance_websocket_api_manager = BinanceWebSocketApiManager(
-            exchange="binance.com")
+        
+    if(error == 0):        
+        binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com")
         binance_websocket_api_manager.create_stream(
             ['miniTicker'], ['btcusdt'])
-        while True:
+        while True:           
             stream = binance_websocket_api_manager.pop_stream_data_from_stream_buffer()
             if stream:
                 jsonstream = json.loads(stream)
-                data = jsonstream.get('data')
+                data = jsonstream.get('data') 
                 if data:
-                    
                     resdisdata = rclient.get('last_sent_price')
                     if (resdisdata):
                         last_sent_price = ast.literal_eval(resdisdata)
@@ -352,32 +340,23 @@ async def main():
                     mark_price = float(data['c'])
                     print("Last Price; ", last_sent_price)
                     print("Mark Price; ", mark_price)
-                    rclient.set('BTC_PRICE', str(mark_price))
-
+                    
                     print(0.99*last_sent_price)
                     print(1.01*last_sent_price)
 
                     if rclient.get("sent_flag") == None:
-                        fixtureData = rclient.get('fixtureStarted')
-                        if (fixtureData):
-                            fixtureId = ast.literal_eval(fixtureData)
-                            print("Fixture Id; ", fixtureId)
-                            await calculate(input_data, mark_price, fixtureId)
-                            rclient.set('last_sent_price', str(mark_price))
-                            rclient.setex("sent_flag", 15, 1)
+                        await calculate(input_data, mark_price)
+                        rclient.set('last_sent_price', str(mark_price))
+                        rclient.setex("sent_flag", 15, 1)
                     elif (mark_price < ((1-float(input_data['Price_change']))*last_sent_price) or mark_price > ((1+float(input_data['Price_change']))*last_sent_price)):
-                        fixtureData = rclient.get('fixtureStarted')
-                        if (fixtureData):
-                            fixtureId = ast.literal_eval(fixtureData)
-                            print("Fixture Id; ", fixtureId)
-                            await calculate(input_data, mark_price, fixtureId)
-                            rclient.set('last_sent_price', str(mark_price))
-                            rclient.setex("sent_flag", 15, 1)
+                        await calculate(input_data, mark_price)
+                        rclient.set('last_sent_price', str(mark_price))
+                        rclient.setex("sent_flag", 15, 1)
                     else:
                         res = {}
                         res['price'] = mark_price
                         rclient.publish('BO-DATA', json.dumps(res))
-
+                    
 
 if __name__ == "__main__":
 
---
+++ cronCreated.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+import json
+import math
+import asyncio
+import requests
+import redis
+import os
+import ast
+from db import db_get_fixture
+import uuid
+import hashlib
+
+USERPOOL = redis.ConnectionPool(
+    host='localhost', port=6379, db=0, decode_responses=True)
+rclient = redis.StrictRedis(connection_pool=USERPOOL, decode_responses=True)
+
+CURR_TIME = datetime.now()
+
+FIXTURE_TIME = CURR_TIME + timedelta(seconds=18000)
+print(FIXTURE_TIME)
+
+fixtures = db_get_fixture(FIXTURE_TIME)
+print(fixtures)
+if(fixtures != None):
+    fixtures = json.loads(fixtures)
+    print(fixtures)
+    print(type(fixtures))
+    print(fixtures[0]['marketEndTime'])
+    print(type(fixtures[0]['marketEndTime']))
+    print(fixtures[0]['id'])
+
+    fixtureCreated = rclient.get('fixtureCreated')
+    print(fixtureCreated)
+    if (fixtureCreated):
+        fixtureId = ast.literal_eval(fixtureCreated)
+    else:
+        fixtureId = None
+
+    if(fixtureId == None or fixtureId != fixtures[0]['id']):
+        res = {}
+
+        seq = str(uuid.uuid4())
+        print(seq)
+
+        res['Timestamp'] = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        res['Seq'] = seq
+        res['Fixture'] = {
+            "Id": fixtures[0]['id'],
+            "StartTime":  datetime.utcfromtimestamp(fixtures[0]['startTime']/1000).strftime('%Y/%m/%d %H:%M:%S'),
+            "MarketEndTime": datetime.utcfromtimestamp(fixtures[0]['marketEndTime']/1000).strftime('%Y/%m/%d %H:%M:%S'),
+            "EndTime": datetime.utcfromtimestamp(fixtures[0]['endTime']/1000).strftime('%Y/%m/%d %H:%M:%S')
+        }
+        print(res)
+
+        # SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
+        # access_key = hashlib.md5((SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
+        # print('ACCESS KEY: ', access_key)
+        # headers = {"content-type": "application/json","oneworks-access-key": access_key}
+        # print(headers)
+        # response = requests.post("http://owapi1.playthefun.com:9130/api/CryptoCurrency/CreateFixture", json=res, headers=headers)
+        # print(response)
+        # if(response.status_code == 200):
+        rclient.set("fixtureCreated", str(fixtures[0]['id']))
