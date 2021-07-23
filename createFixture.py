3f71836cb9ab8d74e5bff6a43d30537d852ea7ca
sbryson630 <sbryson630@gmail.com>
2021-07-23 13:37:08 +0530

    create fixture with parameters

--- createFixture.py
+++ createFixture.py
@@ -1,71 +1,41 @@
-
 from datetime import datetime, timedelta
 import json
-import requests
-import hashlib
+import os
 from db import db_add_fixture
-
-# data = {"Timestamp": "2021/07/06 15:26:00.768","Seq": "d8382c1d-6ce0-433d-9233-e7358b10a70b","Fixture":{"Id": 1625559334,"StartTime": "2021/07/06 03:30:00","MarketEndTime": "2021/07/06 03:40:00","EndTime": "2021/07/06 03:45:00"}}
-
-# SECRET_KEY = '8sxy54vjd3ks5cge'
-
-# access_key = hashlib.md5((SECRET_KEY+str(data)).encode('utf-8')).hexdigest()
-
-# print(access_key)
-
-# headers = {"content-type": "application/json",
-#            "oneworks-access-key": "a37a6a7e82779a3312b880bd5567cd73"}
-
-# response = requests.post("http://owapi1.playthefun.com:9130/api/CryptoCurrency/CreateFixture", json=data, headers=headers)
-# print(response)
-# print(response.json())
-# print(json.dumps(response.json()))
-# print('\n\n')
-
-
-
-# def createFixture():
-
-#     db_add_fixture()
-
 
 def hour_rounder(t):
     # Rounds to nearest hour by adding a timedelta hour if minute >= 30
     return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
             + timedelta(hours=t.minute//30))
 
-STARTTIME = datetime.now()
-STARTTIME = hour_rounder(STARTTIME)
-print(STARTTIME)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+path = dir_path + '/fixture.json'
+#print(path)
+f = open(path)
+data = json.load(f)
 
+start_end_diff = int(data['StartEndDiff'])
+start_market_diff = int(data['StartMarketDiff'])
 
-ENDTIME = STARTTIME + timedelta(hours=24)
-print(ENDTIME)
+if(start_end_diff and start_market_diff):
 
-now = datetime.now()
+    STARTTIME = datetime.now()
+    STARTTIME = hour_rounder(STARTTIME)
+    print(STARTTIME)
 
-#type 1
-while(STARTTIME < ENDTIME):
-    start_time = STARTTIME
-    end_time = start_time + timedelta(minutes=15)
-    market_end_time =  end_time - timedelta(minutes=5)
-    # print(end_time.minute)
-    # if (end_time.minute != 0):
-    print(start_time, market_end_time, end_time)
-    db_add_fixture(1, start_time, market_end_time, end_time)
+    ENDTIME = STARTTIME + timedelta(hours=24)
+    print(ENDTIME)
 
-    STARTTIME = end_time
+    now = datetime.now()
 
-STARTTIME = datetime.now()
-STARTTIME = hour_rounder(STARTTIME)
-print(STARTTIME)
+    #type 1
+    while(STARTTIME < ENDTIME):
+        start_time = STARTTIME
+        end_time = start_time + timedelta(minutes=start_end_diff)
+        market_end_time =  start_time + timedelta(minutes=start_market_diff)
+        # print(end_time.minute)
 
-# # type 2
-# while(STARTTIME < ENDTIME):
-#     start_time = STARTTIME
-#     end_time = start_time + timedelta(hours=1)
-#     market_end_time = end_time - timedelta(minutes=5)
-#     print(start_time, market_end_time, end_time)
-#     db_add_fixture(2, start_time, market_end_time, end_time)
+        print(start_time, market_end_time, end_time)
+        db_add_fixture(1, start_time, market_end_time, end_time)
 
-#     STARTTIME = end_time
+        STARTTIME = end_time
