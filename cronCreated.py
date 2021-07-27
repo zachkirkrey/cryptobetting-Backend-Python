c20600753054d9fef57110165e09a57a85f41a15
pavs <pavs94@gmail.com>
2021-07-27 14:27:45 +0530

    retry code

--- cronCreated.py
+++ cronCreated.py
@@ -29,37 +29,39 @@ if(fixtures != None):
     print(fixtures[0]['marketEndTime'])
     print(type(fixtures[0]['marketEndTime']))
     print(fixtures[0]['id'])
+    print('STATUS: ', fixtures[0]['status'])
 
-    fixtureCreated = rclient.get('fixtureCreated')
-    print(fixtureCreated)
-    if (fixtureCreated):
-        fixtureId = ast.literal_eval(fixtureCreated)
-    else:
-        fixtureId = None
+    if(fixtures[0]['status'] != 'CREATED'):
+        fixtureCreated = rclient.get('fixtureCreated')
+        print(fixtureCreated)
+        if (fixtureCreated):
+            fixtureId = ast.literal_eval(fixtureCreated)
+        else:
+            fixtureId = None
 
-    if(fixtureId == None or fixtureId != fixtures[0]['id']):
-        res = {}
+        if(fixtureId == None or fixtureId != fixtures[0]['id']):
+            res = {}
 
-        seq = str(uuid.uuid4())
-        print(seq)
+            seq = str(uuid.uuid4())
+            print(seq)
 
-        res['Timestamp'] = (datetime.now() + timedelta(hours=8)).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
-        res['Seq'] = seq
-        res['Fixture'] = {
-            "Id": fixtures[0]['id'],
-            "StartTime":  datetime.utcfromtimestamp(fixtures[0]['startTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S'),
-            "MarketEndTime": datetime.utcfromtimestamp(fixtures[0]['marketEndTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S'),
-            "EndTime": datetime.utcfromtimestamp(fixtures[0]['endTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S')
-        }
-        print(res)
+            res['Timestamp'] = (datetime.now() + timedelta(hours=8)).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
+            res['Seq'] = seq
+            res['Fixture'] = {
+                "Id": fixtures[0]['id'],
+                "StartTime":  datetime.utcfromtimestamp(fixtures[0]['startTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S'),
+                "MarketEndTime": datetime.utcfromtimestamp(fixtures[0]['marketEndTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S'),
+                "EndTime": datetime.utcfromtimestamp(fixtures[0]['endTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S')
+            }
+            print(res)
 
-        SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
-        access_key = hashlib.md5((SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
-        print('ACCESS KEY: ', access_key)
-        headers = {"content-type": "application/json","oneworks-access-key": access_key}
-        print(headers)
-        response = requests.post("http://owapi1.playthefun.com:9130/api/CryptoCurrency/CreateFixture", json=res, headers=headers)
-        print(response)
-        if(response.status_code == 200):
-            db_set_fixture_status(fixtures[0]['id'], "CREATED")
-            rclient.set("fixtureCreated", str(fixtures[0]['id']))
+            SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
+            access_key = hashlib.md5((SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
+            print('ACCESS KEY: ', access_key)
+            headers = {"content-type": "application/json","oneworks-access-key": access_key}
+            print(headers)
+            response = requests.post("http://owapi1.playthefun.com:9130/api/CryptoCurrency/CreateFixture", json=res, headers=headers)
+            print(response)
+            if(response.status_code == 200):
+                db_set_fixture_status(fixtures[0]['id'], "CREATED")
+                rclient.set("fixtureCreated", str(fixtures[0]['id']))
