6a0025b13410eab2e8d0e998167e8132f760e130
pavs <pavs94@gmail.com>
2021-08-02 15:21:51 +0530

    websocket ping pong

--- server.js
+++ server.js
@@ -15,11 +15,16 @@ var client = redis.createClient(REDIS_SERVER);
 // Create & Start the WebSocket server
 const server = new WebSocket.Server({ port: WEB_SOCKET_PORT });
 
+function noop() { }
 
+function heartbeat() {
+    this.isAlive = true;
+}
 
 // Register event for client connection
 server.on('connection', function connection(ws) {
-
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
     // client.get("BO-DATA", (err, data) => {
     //     if (err) {
     //         console.error(err);
@@ -32,18 +37,34 @@ server.on('connection', function connection(ws) {
 
     // });
 
+    ws.on('message', function (message) {
+        // console.log(JSON.parse(message));
+        var msg = JSON.parse(message);
+        // console.log(typeof msg);
+        if (msg.hasOwnProperty('ping')) {
+            ws.send(JSON.stringify({ "pong": msg['ping'] }));
+        }
+    });
+
     // broadcast on web socket when receving a Redis PUB/SUB Event
     redisClient.on('message', function (channel, message) {
         console.log(message);
-        if ('ping' in message) {
-            res = { "pong": message['ping'] }
-            ws.send(res);
-        }
-        else {
-            ws.send(message);
-        }
+        ws.send(message);        
     })
 
+});
+
+const interval = setInterval(function ping() {
+    server.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping(noop);
+    });
+}, 30000);
+
+server.on('close', function close() {
+    clearInterval(interval);
 });
 
 console.log("WebSocket server started at ws://locahost:" + WEB_SOCKET_PORT);
