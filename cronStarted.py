e688f510e4823d9ceadd38715127f75f3fab5a73
zachkirkrey <zachkirkrey@gmail.com>
2021-08-20 11:13:21 +0530

    added error handling

--- cronStarted.py
+++ cronStarted.py
@@ -17,49 +17,55 @@ rclient = redis.StrictRedis(connection_pool=USERPOOL, decode_responses=True)
 CURR_TIME = datetime.now()
 print(CURR_TIME)
 
-fixtures = db_get_started_fixture(CURR_TIME)
-print(fixtures)
-if(fixtures != None):
-    fixtures = json.loads(fixtures)
+try:
+    fixtures = db_get_started_fixture(CURR_TIME)
     print(fixtures)
-    print(type(fixtures))
-    print(fixtures[0]['marketEndTime'])
-    print(type(fixtures[0]['marketEndTime']))
-    print(fixtures[0]['id'])
-    print(fixtures[0]['status'])
+    if(fixtures != None):
+        fixtures = json.loads(fixtures)
+        print(fixtures)
+        print(type(fixtures))
+        print(fixtures[0]['marketEndTime'])
+        print(type(fixtures[0]['marketEndTime']))
+        print(fixtures[0]['id'])
+        print(fixtures[0]['status'])
 
-    if(fixtures[0]['status'] == 'CREATED'):
+        if(fixtures[0]['status'] == 'CREATED'):
 
-        fixtureStarted = rclient.get('fixtureStarted')
-        print(fixtureStarted)
-        if (fixtureStarted):
-            fixtureId = ast.literal_eval(fixtureStarted)
-        else:
-            fixtureId = None
+            fixtureStarted = rclient.get('fixtureStarted')
+            print(fixtureStarted)
+            if (fixtureStarted):
+                fixtureId = ast.literal_eval(fixtureStarted)
+            else:
+                fixtureId = None
 
-        if(fixtureId == None or fixtureId != fixtures[0]['id']):
-            print('PUSH FIXTURE ID TO JSON')
-            print(fixtures[0]['id'])
-            db_set_fixture_status(fixtures[0]['id'], "STARTED")
-            rclient.set("fixtureStarted", str(fixtures[0]['id']))
-            rclient.sadd("fixtureId", str(fixtures[0]['id']))
+            if(fixtureId == None or fixtureId != fixtures[0]['id']):
+                print('PUSH FIXTURE ID TO JSON')
+                print(fixtures[0]['id'])
+                db_set_fixture_status(fixtures[0]['id'], "STARTED")
+                rclient.set("fixtureStarted", str(fixtures[0]['id']))
+                rclient.sadd("fixtureId", str(fixtures[0]['id']))
+except Exception as e:
+    print(e)
 
-endfixtures = db_get_last_started_fixture(CURR_TIME)
-print(endfixtures)
-if(endfixtures != None):
-    fixtures = json.loads(endfixtures)
-    print(fixtures)
-    print(type(fixtures))
-    print(fixtures[0]['marketEndTime'])
-    print(type(fixtures[0]['marketEndTime']))
-    print(fixtures[0]['id'])
+try:
+    endfixtures = db_get_last_started_fixture(CURR_TIME)
+    print(endfixtures)
+    if(endfixtures != None):
+        fixtures = json.loads(endfixtures)
+        print(fixtures)
+        print(type(fixtures))
+        print(fixtures[0]['marketEndTime'])
+        print(type(fixtures[0]['marketEndTime']))
+        print(fixtures[0]['id'])
 
 
-    fixtureId = fixtures[0]['id']
+        fixtureId = fixtures[0]['id']
 
-    if(fixtureId != None):
-        print('STOP FIXTURE')
-        print(fixtureId)
-        print("Contents of the Redis set:")
-        print(rclient.smembers("fixtureId"))
-        rclient.srem("fixtureId", fixtureId)
+        if(fixtureId != None):
+            print('STOP FIXTURE')
+            print(fixtureId)
+            print("Contents of the Redis set:")
+            print(rclient.smembers("fixtureId"))
+            rclient.srem("fixtureId", fixtureId)
+except Exception as e:
+    print(e)
