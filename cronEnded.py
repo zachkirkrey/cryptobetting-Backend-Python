89229027d2e893fd65377fd10a46b230c1b5855b
pavs <pavs94@gmail.com>
2021-08-31 19:15:39 +0530

    changes in create and end fixture

--- cronEnded.py
+++ cronEnded.py
@@ -35,67 +35,70 @@ print(type(fixtures))
 print(fixtures)
 if(fixtures != None):
     fixtures = json.loads(fixtures)
-    print(fixtures[0]['marketEndTime'])
-    print(type(fixtures[0]['marketEndTime']))
-    print(fixtures[0]['id'])
-    print(fixtures[0]['status'])
+    for fixture in fixtures:
+        print(fixture)
+        print(fixture['marketEndTime'])
+        print(type(fixture['marketEndTime']))
+        print(fixture['id'])
+        print(fixture['status'])
 
-    if(fixtures[0]['status'] == 'STARTED'):
+        if(fixture['status'] == 'STARTED'):
 
-        fixtureEnded = rclient.get('fixtureEnded')
-        print(fixtureEnded)
-        if (fixtureEnded):
-            fixtureId = ast.literal_eval(fixtureEnded)
-        else:
-            fixtureId = None
+            fixtureEnded = rclient.get('fixtureEnded')
+            print(fixtureEnded)
+            if (fixtureEnded):
+                fixtureId = ast.literal_eval(fixtureEnded)
+            else:
+                fixtureId = None
 
-        if(fixtureId == None or fixtureId != fixtures[0]['id']):
-            print('CALL END FIXTURE API AND COMMUNICATE THE RESULT')
-            print(fixtures[0]['id'])
+            if(fixtureId == None or fixtureId != fixture['id']):
+                print('CALL END FIXTURE API AND COMMUNICATE THE RESULT')
+                print(fixture['id'])
 
-            res = {}
+                res = {}
 
-            seq = str(uuid.uuid4())
-            print(seq)
+                seq = str(uuid.uuid4())
+                print(seq)
 
-            price = 0
-            resdisdata = rclient.get('BTC_PRICE')
-            if (resdisdata):
-                price = ast.literal_eval(resdisdata)
+                price = 0
+                resdisdata = rclient.get('BTC_PRICE')
+                if (resdisdata):
+                    price = ast.literal_eval(resdisdata)
 
-            if(price > 0):
-                res['Timestamp'] = (datetime.now() + timedelta(hours=8)).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
-                res['Seq'] = seq
+                if(price > 0):
+                    res['Timestamp'] = (datetime.now() + timedelta(hours=8)).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
+                    res['Seq'] = seq
 
-                res['Fixture'] = {
-                    "Id": fixtures[0]['id'],
-                    "Price": price,
-                    "Status": 0
-                }
+                    res['Fixture'] = {
+                        "Id": fixture['id'],
+                        "Price": price,
+                        "Status": 0
+                    }
 
-                print(res)
-            
-                try:
-                    fixture_end_price = db_get_fixture_end_price(fixtures[0]['id'])
-                    if(fixture_end_price == None):
-                        db_set_fixture_price(fixtures[0]['id'], price)
+                    print(res)
+                
+                    try:
+                        fixture_end_price = db_get_fixture_end_price(
+                            fixture['id'])
+                        if(fixture_end_price == None):
+                            db_set_fixture_price(fixture['id'], price)
 
-                    SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
-                    access_key = hashlib.md5(
-                        (SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
-                    print('ACCESS KEY: ', access_key)
-                    headers = {"content-type": "application/json",
-                                "oneworks-access-key": access_key}
-                    print(headers)
-                    response = requests.post(
-                        "http://owapi1.playthefun.com:9130/api/CryptoCurrency/EndFixture", json=res, headers=headers)
-                    print(response)
-                    logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - EndFixture - "+str(
-                        fixtures[0]['id'])+" - Request - "+str(res)+" - Response - "+str(response)+" - " + str(response.text))
-                    if(response.status_code == 200):
-                        db_set_fixture_status(fixtures[0]['id'], "ENDED")
-                        rclient.set("fixtureEnded", str(fixtures[0]['id']))
-                        # rclient.delete("fixtureId")             
-                except Exception as e:
-                    logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - EndFixture - "+str(
-                        fixtures[0]['id'])+" - Request - "+str(res)+" - Response - "+str(e))
+                        SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
+                        access_key = hashlib.md5(
+                            (SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
+                        print('ACCESS KEY: ', access_key)
+                        headers = {"content-type": "application/json",
+                                    "oneworks-access-key": access_key}
+                        print(headers)
+                        response = requests.post(
+                            "http://owapi1.playthefun.com:9130/api/CryptoCurrency/EndFixture", json=res, headers=headers)
+                        print(response)
+                        logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - EndFixture - "+str(
+                            fixture['id'])+" - Request - "+str(res)+" - Response - "+str(response)+" - " + str(response.text))
+                        if(response.status_code == 200):
+                            db_set_fixture_status(fixture['id'], "ENDED")
+                            rclient.set("fixtureEnded", str(fixture['id']))
+                            # rclient.delete("fixtureId")             
+                    except Exception as e:
+                        logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - EndFixture - "+str(
+                            fixture['id'])+" - Request - "+str(res)+" - Response - "+str(e))
