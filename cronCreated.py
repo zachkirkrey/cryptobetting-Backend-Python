9b11f9410e6717039aebb8f87cb78be6036b857c
pavs <pavs94@gmail.com>
2021-08-31 21:01:02 +0530

    create fixture retry

--- cronCreated.py
+++ cronCreated.py
@@ -25,10 +25,10 @@ handler = TimedRotatingFileHandler('./owapi1.log',
                                    interval=7,
                                    backupCount=3)
 logger.addHandler(handler)
-    
+
 CURR_TIME = datetime.now()
 
-FIXTURE_TIME = CURR_TIME + timedelta(seconds=18000)
+FIXTURE_TIME = CURR_TIME + timedelta(minutes=15)
 print(FIXTURE_TIME)
 
 fixtures = db_get_fixture(FIXTURE_TIME)
@@ -37,46 +37,53 @@ if(fixtures != None):
     fixtures = json.loads(fixtures)
     print(fixtures)
     print(type(fixtures))
-    print(fixtures[0]['marketEndTime'])
-    print(type(fixtures[0]['marketEndTime']))
-    print(fixtures[0]['id'])
-    print('STATUS: ', fixtures[0]['status'])
+    for fixture in fixtures:
+        print(fixture['marketEndTime'])
+        print(type(fixture['marketEndTime']))
+        print(fixture['id'])
+        print('STATUS: ', fixture['status'])
 
-    if(fixtures[0]['status'] != 'CREATED'):
-        fixtureCreated = rclient.get('fixtureCreated')
-        print(fixtureCreated)
-        if (fixtureCreated):
-            fixtureId = ast.literal_eval(fixtureCreated)
-        else:
-            fixtureId = None
+        if(fixture['status'] != 'CREATED'):
+            fixtureCreated = rclient.get('fixtureCreated')
+            print(fixtureCreated)
+            if (fixtureCreated):
+                fixtureId = ast.literal_eval(fixtureCreated)
+            else:
+                fixtureId = None
 
-        if(fixtureId == None or fixtureId != fixtures[0]['id']):
-            res = {}
+            if(fixtureId == None or fixtureId != fixture['id']):
+                res = {}
 
-            seq = str(uuid.uuid4())
-            print(seq)
+                seq = str(uuid.uuid4())
+                print(seq)
 
-            res['Timestamp'] = (datetime.now() + timedelta(hours=8)).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
-            res['Seq'] = seq
-            res['Fixture'] = {
-                "Id": fixtures[0]['id'],
-                "StartTime":  datetime.utcfromtimestamp(fixtures[0]['startTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S'),
-                "MarketEndTime": datetime.utcfromtimestamp(fixtures[0]['marketEndTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S'),
-                "EndTime": datetime.utcfromtimestamp(fixtures[0]['endTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S')
-            }
-            print(res)
-            try:
-                SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
-                access_key = hashlib.md5((SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
-                print('ACCESS KEY: ', access_key)
-                headers = {"content-type": "application/json","oneworks-access-key": access_key}
-                print(headers)
-                response = requests.post("http://owapi1.playthefun.com:9130/api/CryptoCurrency/CreateFixture", json=res, headers=headers)
-                print(response)
-                logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - CreateFixture - "+str(fixtures[0]['id'])+" - Request - "+str(res)+" - Response - "+str(response)+" - " + str(response.text) )
-                print(type(response))
-                if(response.status_code == 200 and 'ErrorCode' not in response):
-                    db_set_fixture_status(fixtures[0]['id'], "CREATED")
-                    rclient.set("fixtureCreated", str(fixtures[0]['id']))
-            except Exception as e:
-                logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - CreateFixture - "+str(fixtures[0]['id'])+" - Request - "+str(res)+" - Response - "+str(e))
+                res['Timestamp'] = (
+                    datetime.now() + timedelta(hours=8)).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
+                res['Seq'] = seq
+                res['Fixture'] = {
+                    "Id": fixture['id'],
+                    "StartTime":  datetime.utcfromtimestamp(fixture['startTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S'),
+                    "MarketEndTime": datetime.utcfromtimestamp(fixture['marketEndTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S'),
+                    "EndTime": datetime.utcfromtimestamp(fixture['endTime']/1000).astimezone(pytz.timezone('America/Antigua')).strftime('%Y/%m/%d %H:%M:%S')
+                }
+                print(res)
+                try:
+                    SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
+                    access_key = hashlib.md5(
+                        (SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
+                    print('ACCESS KEY: ', access_key)
+                    headers = {"content-type": "application/json",
+                               "oneworks-access-key": access_key}
+                    print(headers)
+                    response = requests.post(
+                        "http://owapi1.playthefun.com:9130/api/CryptoCurrency/CreateFixture", json=res, headers=headers)
+                    print(response)
+                    logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - CreateFixture - "+str(
+                        fixture['id'])+" - Request - "+str(res)+" - Response - "+str(response)+" - " + str(response.text))
+                    print(type(response))
+                    if(response.status_code == 200 and 'ErrorCode' not in response):
+                        db_set_fixture_status(fixture['id'], "CREATED")
+                        rclient.set("fixtureCreated", str(fixture['id']))
+                except Exception as e:
+                    logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - CreateFixture - "+str(
+                        fixture['id'])+" - Request - "+str(res)+" - Response - "+str(e))
