2dd351fe51aa0b51ff34fbf8986f981ddecd66fe
zachkirkrey <zachkirkrey@gmail.com>
2021-09-07 13:52:38 +0530

    EndMarket api

--- cronStarted.py
+++ cronStarted.py
@@ -10,10 +10,21 @@ import ast
 from db import db_get_started_fixture, db_set_fixture_status, db_get_last_started_fixture
 import uuid
 import hashlib
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 USERPOOL = redis.ConnectionPool(
     host='localhost', port=6379, db=0, decode_responses=True)
 rclient = redis.StrictRedis(connection_pool=USERPOOL, decode_responses=True)
+
+logger = logging.getLogger("Rotating Log")
+logger.setLevel(logging.INFO)
+
+handler = TimedRotatingFileHandler('./owapi1.log',
+                                   when="d",
+                                   interval=7,
+                                   backupCount=3)
+logger.addHandler(handler)
 
 CURR_TIME = datetime.now()
 print(CURR_TIME)
@@ -72,8 +83,40 @@ try:
             for value in fixtureIds:
                 print(value)
                 val = int(value)
-                if(val <= fixtureId):
-                    rclient.srem("fixtureId", val)
+                if(val <= fixtureId):          
+                    res = {}
+
+                    seq = str(uuid.uuid4())
+                    print(seq)
+
+                    res['Timestamp'] = (
+                        datetime.now() + timedelta(hours=8)).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
+                    res['Seq'] = seq
+                    res['Fixture'] = {
+                        "Id": val
+                    }
+                    print(res)
+                    try:
+                        SECRET_KEY = os.getenv('OWAPI_SECRET_KEY')
+                        access_key = hashlib.md5(
+                            (SECRET_KEY+json.dumps(res)).encode('utf-8')).hexdigest()
+                        print('ACCESS KEY: ', access_key)
+                        headers = {"content-type": "application/json",
+                                "oneworks-access-key": access_key}
+                        print(headers)
+                        response = requests.post(
+                            "http://owapi1.playthefun.com:9130/api/CryptoCurrency/EndMarket", json=res, headers=headers)
+                        print(response)
+                        logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - EndMarket - "+str(
+                            val)+" - Request - "+str(res)+" - Response - "+str(response)+" - " + str(response.text))
+                        print(type(response))
+                        if(response.status_code == 200 and 'ErrorCode' not in response):
+                            rclient.srem("fixtureId", val)
+                    except Exception as e:
+                        logger.info(str(CURR_TIME.strftime("%m/%d/%Y, %H:%M:%S"))+" - EndMarket - "+str(
+                            val)+" - Request - "+str(res)+" - Response - "+str(e))
+
+
 
 except Exception as e:
     print(e)
