d3b71c4541f452479349ee959830c83530d48d7a
EduRuizzo <edu.ruizzo@gmail.com>
2021-09-30 15:01:22 +0200

    docker: supervisord working

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 service cron start
+supervisord -c sup-docker.conf
 gunicorn -w 3 -t 2 -b 0.0.0.0:$BACKEND_PORT --timeout 15000 --max-requests 1000 app:app
 # /usr/bin/supervisord
-# nohup supervisord -c sup-docker.conf &
